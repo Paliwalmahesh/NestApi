1486e9f87cca49ad954a61c6d1d0319a4145cd45
visrut <visrutnavadiya111+github@gmail.com>
2023-02-16 16:24:50 +0530

    pokemon added

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
+import { CreatePokemonDTO } from './dto/pokemonDTO';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -8,5 +9,10 @@ export class PokemonController {
   @Get()
   findAll(): any {
     return this.pokemonService.findAllPokemons();
+  }
+
+  @Post()
+  save(@Body() createPokemonDTO: CreatePokemonDTO) {
+    return this.pokemonService.savePokemon(createPokemonDTO);
   }
 }
